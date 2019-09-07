cddfbb354f78097bde2abc72068add611d17090d
TheOnlySmartBoy <o2jose43@gmail.com>
2019-09-07 10:04:14 +0300

    Added admin lte resources

--- app/User.php
+++ app/User.php
@@ -2,12 +2,16 @@
 
 namespace App;
 
+use Carbon\Carbon;
+use Hash;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
+
     use Notifiable;
 
     /**
@@ -36,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles() {
+        return $this->belongsToMany(Role::class);
+    }
+
 }
