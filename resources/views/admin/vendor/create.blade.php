74444d8783745a9a60d7c03c7dcc442e5f521417
TheOnlySmartBoy <o2jose43@gmail.com>
2019-09-11 12:57:08 +0300

    Completed view vendors

--- resources/views/admin/vendor/create.blade.php
+++ resources/views/admin/vendor/create.blade.php
@@ -1,8 +1,83 @@
-<?php
+@extends('layouts.adminlayout.admin_design')
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Vendors</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{url('/admin/dashboard')}}">Home</a></li>
+                        <li class="breadcrumb-item"> <a href="{{url('/admin/vendor')}}">Vendors</a></li>
+                        <li class="breadcrumb-item active">Create</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">                         
+            <!-- Center column -->               
+            <!-- general form elements disabled -->
+            <div class="card card-warning">
+                <div class="card-header">
+                    <h3 class="card-title">Create Vendors form</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <form role="form">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <!-- text input -->
+                                <div class="form-group">
+                                    <label>Text</label>
+                                    <input type="text" class="form-control" placeholder="Enter ...">
+                                </div>
+                            </div>
+                        </div>
+                          <div class="row">
+                            <div class="col-sm-6">
+                                <!-- text input -->
+                                <div class="form-group">
+                                    <label>Text</label>
+                                    <input type="text" class="form-control" placeholder="Enter ...">
+                                </div>
+                            </div>
+                        </div>
+                          <div class="row">
+                            <div class="col-sm-6">
+                                <!-- text input -->
+                                <div class="form-group">
+                                    <label>Text</label>
+                                    <input type="text" class="form-control" placeholder="Enter ...">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <!-- select -->
+                                <div class="form-group">
+                                    <label>Select</label>
+                                    <select class="form-control">
 
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.card-body -->
+            </div>
+
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+@endsection
