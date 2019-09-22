45482b1bf441e2c66bdd7d5f3364c29023a45b62
TheOnlySmartBoy <o2jose43@gmail.com>
2019-09-22 11:36:59 +0300

    Updated Sidebar links

--- app/Http/Controllers/ZoneController.php
+++ app/Http/Controllers/ZoneController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Zone;
 
 use Illuminate\Http\Request;
 
-class ZoneController extends Controller
-{
-    //
+class ZoneController extends Controller {
+
+    public function index() {
+        if (Session::has('adminSession')) {
+            $zones = Zone::get();
+            return view('admin.zone.index')->with(compact('zones'));
+        } else {
+            return redirect()->back()->with('flash_message_error', 'Access Denied');
+        }
+    }
+
 }
