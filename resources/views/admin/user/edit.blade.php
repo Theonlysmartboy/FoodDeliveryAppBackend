8fe3049a54caad1dbbafc66538a5a5ce8c92871a
TheOnlySmartBoy <o2jose43@gmail.com>
2020-01-01 17:58:39 +0300

    Housekeeping

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -39,7 +39,7 @@
             <!-- general form elements disabled -->
             <div class="card card-warning">
                 <div class="card-header">
-                    <h3 class="card-title">Create Users form</h3>
+                    <h3 class="card-title">Update Users form</h3>
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
